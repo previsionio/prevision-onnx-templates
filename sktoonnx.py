c2d769216cd3c215774910095138fe3f5e2466e4
Arnold Zephir <arnold.zephir@prevision.io>
2021-10-04 15:04:44 +0200

    doc

--- sktoonnx.py
+++ sktoonnx.py
@@ -15,14 +15,14 @@ from pathlib import Path
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
+logging.info("This script builds a classifier with sklearn and save it to onnx format ")
+logging.info("it generates the onnx file, the config file, trainset and holdout for reproducibility")
 
 logging.info("Loading a small dataset for test")
 PATH="data"
 INPUT_PATH=join(PATH, "transaction_fraud_subset.csv")
 OUTPUT_PATH=join(PATH,"assets")
-
 Path(OUTPUT_PATH).mkdir(parents=True, exist_ok=True)
-
 df = pd.read_csv(INPUT_PATH).set_index('TransactionID')
 
 
@@ -50,21 +50,21 @@ scaler = StandardScaler()
 Xnum = scaler.fit_transform(X[num_feat])
 
 
-logging.info
+logging.info("Rebuilding the final dataset")
 ff = Xcat.join(pd.DataFrame(Xnum,columns=num_feat, index=Xcat.index))
 ff.replace([np.inf, -np.inf], np.nan, inplace=True)
 ff = ff.fillna(-1)
 
-#Lets convert everything to float for easier onnx management
+logging.info("Converting all data to Numpy Float 32")
+logging.info("in order to have a Onnx modele with only one data type")
 ff = ff.astype(np.float32)
+logging.info("The dataset is an array of Float 32")
 
 
-
+logging.info( "Split the dataset and save for holdout validation")
 X_train, X_test, y_train, y_test = train_test_split(ff, y, test_size=0.2, random_state=754)
-
-# Save for later use
 X_train.join(y_train).to_csv(join(OUTPUT_PATH,'trainset_fraud.csv'))
-X_test.join(y_test).to_csv(join(OUTPUT_PATH,'testset_fraud.csv'))
+X_test.join(y_test).to_csv(join(OUTPUT_PATH,'holdout_fraud.csv'))
 
 
 logging.info("********************** Modelisation ***********************************")
@@ -75,18 +75,23 @@ clf.score(X_train, y_train)
 logging.info("********************** Validation ***********************************")
 # Check its not that bad
 yhat=clf.predict(X_test)
-confusion_matrix(y_test, yhat)
 
 
+cm = confusion_matrix(y_test, yhat)
+logging.info("Original Model Confusion matrix")
+logging.info(cm[0])
+logging.info(cm[1])
 
 logging.info("********************** Converting to onnx ***********************************")
 from skl2onnx import convert_sklearn
 from skl2onnx.common.data_types import FloatTensorType
 
-
+logging.info("Datas are a tensor of float_input whose shape is [None, X_train numbers of columns without target]")
 initial_type = [('float_input', FloatTensorType([None,  np.array(X_train).shape[1]]))]
+logging.info("Converting")
 onx = convert_sklearn(clf, initial_types=initial_type)
 
+logging.info(f"Saving the model to {join(OUTPUT_PATH,'classif_fraud.onnx')}")
 with open(join(OUTPUT_PATH,"classif_fraud.onnx"), "wb") as f:
     f.write(onx.SerializeToString())
 
@@ -96,13 +101,24 @@ with open(join(OUTPUT_PATH,"classif_fraud.onnx"), "wb") as f:
 logging.info("********************** Checking the onnx file ***********************************")
 import onnxruntime as rt
 import numpy
+logging.info("Loading a previous model")
 sess = rt.InferenceSession(join(OUTPUT_PATH,"classif_fraud.onnx"))
 
 input_name = sess.get_inputs()[0].name
 label_name = sess.get_outputs()[0].name
 
+logging.info("Applyed it on testest")
 cols = list(pd.read_csv(join(OUTPUT_PATH,"testset_fraud.csv"), nrows =1))
 X_test=np.array(pd.read_csv(join(OUTPUT_PATH,'testset_fraud.csv'), usecols =[i for i in cols if i != 'fraude']).set_index('TransactionID'))
 
 
 pred_onx = sess.run([label_name], {input_name: X_test.astype(np.float32)})[0]
+cm = confusion_matrix(y_test, pred_onx)
+logging.info("Onnx Model Confusion matrix")
+logging.info(cm[0])
+logging.info(cm[1])
+
+logging.info("Check the values. Everything ran fine.")
+logging.info("You can grab the model, its configuration file, a trainset and holdout in ")
+logging.info(OUTPUT_PATH)
+exit(0)
